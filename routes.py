db001a964b9726c5c7e98238bdb590dd56f44b80
griimnak <griimnak@gmail.com>
2017-12-29 02:12:17 -0500

    cache class

--- routes.py
+++ routes.py
@@ -1,22 +1,6 @@
-from app import app
+from app import app, cached
 from flask import redirect, url_for, session
 from flask import render_template, make_response, request
-from werkzeug.contrib.cache import SimpleCache
-
-CACHE_TIMEOUT = 300
-cache = SimpleCache()
-class cached(object):
-    def __init__(self, timeout=None):
-        self.timeout = timeout or CACHE_TIMEOUT
-
-    def __call__(self, f):
-        def decorator(*args, **kwargs):
-            response = cache.get(request.path)
-            if response is None:
-                response = f(*args, **kwargs)
-                cache.set(request.path, response, self.timeout)
-            return response
-        return decorator
 
 
 @app.route('/')
