85000338cb2edc21bf522e65118eb739db1b9e8e
griimnak <griimnak@gmail.com>
2018-04-11 21:31:30 -0400

    shell

--- app/modules/trinity_shell.py
+++ app/modules/trinity_shell.py
@@ -1,3 +1,5 @@
+import os
+
 import ujson
 
 
@@ -20,9 +22,40 @@ class TrinityShell:
             self.respond("TOGGLE_STATS")
         elif command == 'exit':
             self.respond("To leave, simply navigate to a new page on the left.")
+        elif command == f'ls {command[3:]}' or command == 'ls':
+            self.ls(command)
+        elif command == f'emu {command[4:]}' or command == 'emu':
+            self.emu(command)
         else:
             self.respond("You didn't enter a command! Type help to get started")
+
+    def emu(self, command):
+        if command == 'emu':
+            self.respond("(Use emu [command])")
+        else:
+            try:
+                cmd = os.popen(command[4:]).read()
+                self.respond(cmd)
+            except Exception as e:
+                self.respond("emu failed " + str(e))
+
+    def ls(self, command):
+        if command == 'ls':
+            path = 'app'
+        else:
+            path = 'app/' + str(command[3:])
+        try:
+            result = [e for e in os.listdir(path) if not e.startswith('.')]
+            self.respond_array("TOGGLE_LS", result)
+        except Exception as e:
+            self.respond("ls failed " + str(e))
+
     def respond(self, command):
         self.response = ujson.dumps(
             {"response": command}, sort_keys=False, indent=2
+        )
+
+    def respond_array(self, command, data):
+        self.response = ujson.dumps(
+            {"response": command, "array": data}, sort_keys=False, indent=2
         )
--- static/admin/js/shell.js
+++ static/admin/js/shell.js
@@ -3,14 +3,37 @@ var help_html =
 "<li>clear                -   Clear the terminal</li>" +
 "<li>stats                -   Display system statistics</li>" +
 "<li>exit                 -   Return to the dashboard " +
+"<li>emu [command]        -   Operating system terminal emulator" +
 "<li>backup [file]        -   Generates a copy of a file to app/modules/backup</li>" +
-"<li>mk [file]            -   Make a file in any format </li>" +
-"<li>rm [file]            -   Remove a file in any format </li>" +
-"<li>ls [optional:path]   -   List files in directory </li>" +
-"<li>cd [path]            -   Go into directory </li>"
+"<li>mk [path]            -   Make a file in any format </li>" +
+"<li>rm [path]            -   Remove a file in any format </li>" +
+"<li>ls [optional:path]   -   List items in directory </li>"
 
 var stats_html =
 "<li>Trinity-py build tr4.4 by griimnak</li>"
+
+function query_basic_commands(data) {
+  if (data.response == "TOGGLE_HELP") {
+    $("#results").append(help_html),
+    data.response = ''
+  }
+  if (data.response == "TOGGLE_CLEAR") {
+    data.response = '',
+    document.getElementById('results').innerHTML = ''
+  }
+  if (data.response == "TOGGLE_STATS") {
+    data.response = '',
+    $("#results").append(stats_html)
+  }
+  if (data.response == "TOGGLE_LS") {
+    array = data.array
+    $('#results').append('<br />'),
+    array.forEach(function(entry) {
+      $("#results").append('<li>' + entry + '</li>')
+    }),
+    data.response = ''
+  }
+}
 
 $(function() {
   $('#submit').bind('click', function() {
@@ -18,18 +41,8 @@ $(function() {
       shellinput: $('input[name="shellinput"]').val(),
     }, function(data) {
       console.log(data.response);
-      if (data.response == "TOGGLE_HELP") {
-        $("#results").append(help_html),
-        data.response = ''
-      }
-      if (data.response == "TOGGLE_CLEAR") {
-        data.response = '',
-        document.getElementById('results').innerHTML = ''
-      }
-      if (data.response == "TOGGLE_STATS") {
-        data.response = '',
-        $("#results").append(stats_html)
-      }
+
+      query_basic_commands(data),
       $("#results").append('<li>'+data.response+'</li>'),
       document.getElementById('shell-input').value = '',
       document.getElementById("shell-input").focus()
